1048ecf9f2f6b5c349e4a84e82078a7a85fecd9c
MerzoukFatimaZohra <merzoukfatimazahraa@gmail.com>
2023-09-15 11:40:44 +0100

    feat(index): fetch data

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,43 @@
+import { fetchData } from "@/Utility/api"
 import Image from "next/image"
 
-export default function Home() {
+export default function Home({ data }) {
+  const imageUrl = `https://image.tmdb.org/t/p/original${data.results[0].backdrop_path}`
   return (
     <div>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <h1>{JSON.stringify(data.results[0].backdrop_path)}</h1>
+      <div className="w-full carousel box">
+        <div className="carousel-item w-full">
+          <Image
+            src={imageUrl}
+            className="w-full h-screen"
+            alt="Tailwind CSS Carousel component"
+            width={0}
+            height={0}
+            sizes="100vw"
+            quality={100}
+          />
+        </div>
+        <div className="carousel-item w-full">
+          <Image
+            src="/images/Color-Palette10-1.png"
+            className="w-full h-screen"
+            alt="Tailwind CSS Carousel component"
+            width={0}
+            height={0}
+            sizes="100vw"
+            quality={100}
+          />
+        </div>
+      </div>
     </div>
   )
+}
+export async function getStaticProps() {
+  const data = await fetchData("trending/movie/day?language=en-US")
+  return {
+    props: {
+      data,
+    },
+  }
 }
