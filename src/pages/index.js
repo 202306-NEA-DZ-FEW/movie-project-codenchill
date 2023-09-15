7fb93b516cc8da78fd26c784c9ab90a6dd3f3195
Hachem "Timeless" Bouhadede <hachembouhadede@gmail.com>
2023-09-15 04:12:29 +0200

    feat(footer): creat and work on the footer component
    creat a footer component, add the required socials to each member
    
    BREAKING CHANGE: we have a footer
    
    Footer #13

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,11 @@
+import Footer from "@/components/Footer/Footer"
 import NewNav from "@/components/Navbar/Navbar"
 
 export default function Home() {
-  return <NewNav />
+  return (
+    <>
+      <NewNav />
+      <Footer />
+    </>
+  )
 }
