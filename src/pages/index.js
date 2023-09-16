940a948f575773d375397a563b668e5ebf0b4d9c
Hachem "Timeless" Bouhadede <hachembouhadede@gmail.com>
2023-09-16 03:19:34 +0200

    feat(nav available everywhere): made the nav and footer available across the document
    imported the nav and footer to the _documnet.js so it set a s template across the website
    
    BREAKING CHANGE: eveyoone get a nav access eveywhere, and a footer
    
    NavBar Component #3

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,10 +2,5 @@ import Footer from "@/components/Footer/Footer"
 import NewNav from "@/components/Navbar/Navbar"
 
 export default function Home() {
-  return (
-    <>
-      <NewNav />
-      <Footer />
-    </>
-  )
+  return <></>
 }
