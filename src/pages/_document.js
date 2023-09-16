394adcc68eac055cd8859f2131859f94811f4cfe
Hachem "Timeless" Bouhadede <hachembouhadede@gmail.com>
2023-09-16 04:04:38 +0200

    Revert "feat(nav available everywhere): made the nav and footer available across the document"
    This reverts commit 940a948f575773d375397a563b668e5ebf0b4d9c.

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,15 +1,11 @@
 import { Html, Head, Main, NextScript } from "next/document"
-import Footer from "@/components/Footer/Footer"
-import NewNav from "@/components/Navbar/Navbar"
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head />
       <body>
-        <NewNav />
         <Main />
-        <Footer />
         <NextScript />
       </body>
     </Html>
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,5 +2,10 @@ import Footer from "@/components/Footer/Footer"
 import NewNav from "@/components/Navbar/Navbar"
 
 export default function Home() {
-  return <></>
+  return (
+    <>
+      <NewNav />
+      <Footer />
+    </>
+  )
 }
