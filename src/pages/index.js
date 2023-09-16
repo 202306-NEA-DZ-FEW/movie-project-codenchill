ac4e8f181685f217658204c9f1a936835fc79ed9
MerzoukFatimaZohra <merzoukfatimazahraa@gmail.com>
2023-09-16 13:35:14 +0100

    feat(index): add moviecard

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,35 @@
 import { fetchData } from "@/Utility/api"
+import MovieCard from "@/components/moviecard/movie-card"
+import Image from "next/image"
 
 export default function Home({ data }) {
-  return <></>
+  console.log(data[0])
+  return (
+    <ul className="menu menu-vertical lg:menu-horizontal bg-base-200 rounded-box">
+      {data.map((movie, index) => (
+        <MovieCard
+          id={index}
+          key={movie.id}
+          title={movie.title}
+          releaseData={movie.release_date}
+          imageUrl={`https://image.tmdb.org/t/p/original${movie.poster_path}`}
+        />
+      ))}
+      {/*
+        // <Image
+        //    // Remember to add a unique key when mapping
+        //   src={`https://image.tmdb.org/t/p/original${movie.backdrop_path}`}
+        //   className="carousel-item"
+        //   alt="Movie backdrop"
+        //   width={500} // These values are set to 0, which means the browser will determine the size based on the aspect ratio.
+        //   height={500}
+        //   quality={100}
+        // />
+      ))}*/}
+    </ul>
+  )
 }
-export async function getStaticProps() {
+export async function getServerSideProps() {
   const data = await fetchData("trending/movie/day?language=en-US")
   return {
     props: {
