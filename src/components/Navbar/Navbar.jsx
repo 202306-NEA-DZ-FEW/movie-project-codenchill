9c97d7b634e6c5b5dd1debc0d3047bd14f2ad345
MerzoukFatimaZohra <merzoukfatimazahraa@gmail.com>
2023-09-16 10:46:46 +0100

    chore: change things to avoid conflict

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,30 @@
+import React from "react"
+import LogoNTitles from "./LogoNTitle"
+import SearchBar from "./SearchBar"
+import GenresNavButton from "./GenersNavButton"
+import ActorsNavButton from "./ActorsNavbButton"
+import MovieNavButton from "./MovieNavButton"
+
+export default function Navbar() {
+  return (
+    <div className="navbar bg-base-100">
+      {/* Logo and Website Title */}
+      <LogoNTitles />
+
+      {/* Centered Search Bar */}
+      <SearchBar />
+
+      {/* Right Section (Genres, Movies, Actors) */}
+      <div className="flex-none">
+        {/* Dropdown Menu for Genres */}
+        <GenresNavButton />
+
+        {/* Dropdown Menu for Movies */}
+        <MovieNavButton />
+
+        {/* Actors Link */}
+        <ActorsNavButton />
+      </div>
+    </div>
+  )
+}
