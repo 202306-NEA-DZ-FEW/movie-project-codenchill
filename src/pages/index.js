a3fad13406117396cc47f833ac96026731d45a03
MerzoukFatimaZohra <merzoukfatimazahraa@gmail.com>
2023-09-17 14:58:11 +0100

    feat(index home): fetch data and display movie list
    #9

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,31 @@
-import Layout from "@/components/layout/layout"
+import { fetchData } from "@/Utility/api"
+import Layout from "@/components/Layout/Layout"
+import MovieList from "@/components/movielist/movielist"
 
-export default function Home() {
+export default function Home(movies) {
   return (
-    <>
-      <Layout>
-        <h1> Welcome to code and chill</h1>
-      </Layout>
-    </>
-
+    <Layout>
+      <MovieList {...movies} />
+    </Layout>
   )
+}
+
+export async function getServerSideProps(context) {
+  const moviesList = await fetchData("trending/movie/day?language=en-US")
+  const genresList = await fetchData("genre/movie/list?language=en")
+
+  const genres = genresList.genres.reduce((acc, genre) => {
+    acc[genre.id] = genre.name
+    return acc
+  }, {})
+
+  const movies = moviesList.results.map((movie) => {
+    const genreNames = movie.genre_ids.map((genreId) => genres[genreId])
+    return { ...movie, genre_ids: genreNames }
+  })
+  return {
+    props: {
+      movies: movies.slice(0, 20),
+    },
+  }
 }
