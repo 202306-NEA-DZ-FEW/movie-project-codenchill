efbd28e0f76ebf1841dff9ecb144940f4b37dbd6
MerzoukFatimaZohra <merzoukfatimazahraa@gmail.com>
2023-09-17 15:14:17 +0100

    chore: create folders and remove console.log

--- src/pages/actors/TrendingActors.jsx
+++ src/pages/actors/TrendingActors.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react"
 import Link from "next/link"
-import ActorCard from "@/components/actorcard"
+import ActorCard from "@/components/actorcard/actorcard"
 
 export default function TrendingActors() {
   const [actors, setActors] = useState([])
