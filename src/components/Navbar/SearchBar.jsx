e12afb2fac44c0a04ed6398c40f9ac5b85191354
Hachem "Timeless" Bouhadede <hachembouhadede@gmail.com>
2023-09-18 22:36:46 +0200

    fix(searchbar): fix onFocus issues, and restructur of component
    useRef adjusment, EventListener Cleanin after query, onFocus works as intended

--- src/components/Navbar/SearchBar.jsx
+++ src/components/Navbar/SearchBar.jsx
@@ -7,13 +7,8 @@ export default function SearchBar() {
   const [searchResults, setSearchResults] = useState([])
   const [error, setError] = useState(null)
 
-  // Use a ref to keep track of the input element
+  // Use a ref to keep track of the input element and the results list
   const inputRef = useRef(null)
-
-  const handleFocus = () => {
-    const array = [...searchResults].slice(0, 3)
-    setSearchResults(array)
-  }
 
   // Add an event listener to track when the input field loses focus
   useEffect(() => {
@@ -42,22 +37,35 @@ export default function SearchBar() {
     e.preventDefault()
     // console.log("search")
     try {
+      const removedSpecialChars = query.replace(
+        /[!@#$%^&*()_+{}\[\]:;<>,.?~\\\/-]/g,
+        "",
+      )
+
+      // Replace spaces with plus symbols
+      const cleanedQuery = removedSpecialChars.replace(/\s+/g, "+")
       const actorSearchAPI = `search/person?include_adult=false&language=en-US&page=1&query=${encodeURIComponent(
-        query,
+        cleanedQuery,
       )}`
       const movieSearchAPI = `search/movie?include_adult=false&language=en-US&page=1&query=${encodeURIComponent(
-        query,
+        cleanedQuery,
       )}`
-
       const actorData = await fetchData(actorSearchAPI)
       const movieData = await fetchData(movieSearchAPI)
-
       // Extract titles and actor names from the results
-      const titles = movieData.results.map((movie) => movie.title)
-      const actorNames = actorData.results.map((actor) => actor.name)
+      const movieArray = movieData.results.map((movie) => ({
+        title: movie.title,
+        id: movie.id,
+        type: "movie",
+      }))
+      const actorArray = actorData.results.map((actor) => ({
+        title: actor.name,
+        id: actor.id,
+        type: "actor",
+      }))
 
       // Combine titles and actor names into a single array
-      const combinedResults = [...titles, ...actorNames].slice(0, 4) // Get the first four results
+      const combinedResults = [...movieArray, ...actorArray].slice(0, 4) // Get the first four results
 
       setSearchResults(combinedResults)
     } catch (e) {
@@ -67,13 +75,13 @@ export default function SearchBar() {
   }
 
   return (
-    <div className="flex-1 flex v-screen relative">
-      <div className="form-control relative">
-        <form onSubmit={search} onChange={search} onFocus={handleFocus}>
+    <div className="flex-1 flex v-screen relative ">
+      <div className="form-control relative ">
+        <form onSubmit={search} onChange={search} autoComplete="off">
           <input
             type="text"
             placeholder="Search"
-            className="input input-bordered w-full md:w-96 h-10"
+            className="input input-bordered w-full md:w-96 h-10 px-4 py-2 rounded-[32px] border border-gray-500 justify-center items-center gap-12 inline-flex"
             name="search"
             value={query}
             onChange={(e) => setQuery(e.target.value)}
@@ -82,18 +90,29 @@ export default function SearchBar() {
         </form>
       </div>
       {searchResults.length > 0 && (
-        <div className="group">
-          <ul className="absolute left-0 mt-6 space-y-0 bg-white border border-gray-300 max-h-30 md:w-96">
+        <div className="group z-10">
+          <ul className="px-4 py-2 rounded-[32px] absolute left-0 mt-6 space-y-0 bg-white border border-gray-300 max-h-30 md:w-96">
             {searchResults.map((result, index) => (
-              <Link key={index} href={"/movies"}>
-                {console.log("hey fatima")}
-                <li
-                  className="block px-4 py-2 hover:bg-blue-500 hover:text-white"
-                  style={{ width: "100%" }}
+              <li
+                key={index}
+                className="block px-4 py-2 hover:bg-blue-500 hover:text-white"
+                style={{ width: "100%" }}
+                onClick={() => {
+                  // Update searchResults to an empty array
+                  setSearchResults([])
+                  setQuery("")
+                }}
+              >
+                <Link
+                  href={
+                    result.type === "movie"
+                      ? `/movie/${result.id}`
+                      : `/actor/${result.id}`
+                  }
                 >
-                  {result}
-                </li>
-              </Link>
+                  {result.type === "movie" ? result.title : result.name}
+                </Link>
+              </li>
             ))}
           </ul>
         </div>
