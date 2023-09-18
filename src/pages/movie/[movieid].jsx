96d62f20f3a79d0bd54235c62358fcce1dd76947
Katia Ghezali <ghezalikatia@hotmail.com>
2023-09-18 20:29:08 +0100

    style(single movie page): position the components in the right places

--- src/pages/movie/[movieid].jsx
+++ src/pages/movie/[movieid].jsx
@@ -1,53 +1,50 @@
-import React from "react"
-import TrailerPlayer from "@/components/SingleMovieCard/TrailerPlayer"
-import Image from "next/image"
-import { fetchData } from "@/Utility/api"
-import Layout from "@/components/Layout/Layout"
-import MovieList from "@/components/movielist/movielist"
-import ActorList from "@/components/actorlist/actorlist"
+import React from "react";
+import TrailerPlayer from "@/components/SingleMovieCard/TrailerPlayer";
+import Image from "next/image";
+import { fetchData } from "@/Utility/api";
+import Layout from "@/components/Layout/Layout";
+import MovieList from "@/components/MovieList/movielist";
+import ActorList from "@/components/actorlist/actorlist";
 
 export function MovieDetailInfo({ movie, director }) {
   return (
-    <>
-      <div className="card lg:card-side bg-base-100 shadow-xl">
-        <figure>
+    <div className="flex flex-wrap mx-auto max-w-7xl bg-gradient-to-r from-gray-800 to-red-500 p-4 rounded-lg shadow-lg text-white mt-8 flex flex-wrap space-x-0 space-y-2 md:space-x-4">
+      <div className="flex flex-wrap mx-auto max-w-5xl"> 
+        <div className="w-full md:w-1/2 text-center">
+          <h2 className="text-4xl font-bold">{movie.title}</h2>
+          <div className="mt-2">
+            <button className="bg-red-500 text-white px-2 py-1 rounded-lg">
+              {movie.runtime} minutes
+            </button>
+            <button className="bg-red-500 text-white ml-2 px-2 py-1 rounded-lg">
+              {movie.spoken_languages[0] != undefined
+                ? movie.spoken_languages[0].english_name
+                : "unavailable"}
+            </button>
+          </div>
           <Image
             src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
             alt="Movie"
-            width={200}
-            height={200}
+            width={300}
+            height={450}
+            className="mx-auto mt-4 rounded-lg"
           />
-        </figure>
-        <div className="card-body">
-          <h2 className="card-title">{movie.title}</h2>
-          <p>{movie.overview}</p>
-          <p>
-            language:{" "}
-            {movie.spoken_languages[0] != undefined
-              ? movie.spoken_languages[0].english_name
-              : "unavailable"}
-          </p>
-          {movie.production_companies &&
-            movie.production_companies.length > 0 && (
-              <p>
-                Production Companies:{" "}
-                {movie.production_companies
-                  .map((company) => company.name)
-                  .join(", ")}
-              </p>
-            )}
-          <p>Popularity: {movie.popularity}</p>
-          <p>Director: {director}</p>
-
-          <p>Total Votes: {movie.vote_count}</p>
-          <p>Release Date: {movie.release_date}</p>
-          <p>Runtime: {movie.runtime} minutes</p>
-
-          <div className="card-actions justify-end"></div>
+        </div>
+        <div className="w-full md:w-1/2 mt-4 md:mt-40 text-left pl-0">
+          <div className="text-xl my-10">{movie.overview}</div>
+          <p className="text-xl">Director: {director}</p>
+          {movie.production_companies && movie.production_companies.length > 0 && (
+            <p className="text-xl">
+              Production Companies:{" "}
+              {movie.production_companies
+                .map((company) => company.name)
+                .join(", ")}
+            </p>
+          )}
         </div>
       </div>
-    </>
-  )
+    </div>
+  );
 }
 
 export default function MovieDetailPage({
@@ -58,37 +55,46 @@ export default function MovieDetailPage({
   director,
 }) {
   return (
-    <>
-      <Layout>
+    <Layout>
+      <div className="bg-gradient-to-r bg-base-content p-4">
         <MovieDetailInfo movie={movieInfo} director={director} />
         <TrailerPlayer
           youtubeVideoId={
             trailerData != "unavailable" ? (
               trailerData.key
             ) : (
-              <p>Unable to find a trailer</p>
+              <p className="text-white">Unable to find a trailer</p>
             )
           }
         />
-        <ActorList actors={actors} />
-        <MovieList movies={movies} />
-      </Layout>
-    </>
-  )
+        <div className="text-center mt-8">
+          <h2 className="text-2xl font-semibold">Related Movies</h2>
+          
+          <div className="mx-auto w-full max-w-screen-lg">
+            <MovieList movies={movies} />
+          </div>
+          <h2 className="text-2xl font-semibold ">Featured Actors</h2>
+          <div className="mx-auto w-full max-w-screen-lg">
+            <ActorList actors={actors} />
+          </div>
+        </div>
+      </div>
+    </Layout>
+  );
 }
 
 export async function getServerSideProps({ params }) {
-  const movieId = params.movieid
+  const movieId = params.movieid;
   // Fetch movie details using your API
-  const movieInfoData = await fetchData(`movie/${movieId}?language=en-US`)
+  const movieInfoData = await fetchData(`movie/${movieId}?language=en-US`);
   // Fetch movie trailer data using your API
-  const trailerData = await fetchData(`movie/${movieId}/videos?language=en-US`)
-  const similarMovies = await fetchData(`movie/${movieId}/similar`)
-  const movieIds = similarMovies.results.map((movie) => movie.id)
+  const trailerData = await fetchData(`movie/${movieId}/videos?language=en-US`);
+  const similarMovies = await fetchData(`movie/${movieId}/similar`);
+  const movieIds = similarMovies.results.map((movie) => movie.id);
   const movies = await Promise.all(
-    movieIds.map((id) => fetchData(`movie/${id}`)),
-  )
-  const actors = await fetchData(`movie/${movieId}/credits`)
+    movieIds.map((id) => fetchData(`movie/${id}`))
+  );
+  const actors = await fetchData(`movie/${movieId}/credits`);
   return {
     props: {
       movieInfo: movieInfoData,
@@ -100,5 +106,5 @@ export async function getServerSideProps({ params }) {
       actors: actors.cast.slice(0, 5),
       director: actors.crew[0].name,
     },
-  }
+  };
 }
